e95cb79c62e2bca492fbb9d996f054b30cab0b23
kmv1712 <kmv1712@gmail.com>
2017-07-27 07:51:32 +0500

    Add files via upload

--- lesson_4.4/function.php
+++ lesson_4.4/function.php
@@ -7,7 +7,7 @@ function test() {
 
 
 function getBase(){
-// test();
+
 	if (isset($_POST['nameBase'])) {
 		$_POST['nameBase'] = trim(strip_tags($_POST['nameBase']));
     $pdo = new PDO("mysql:host 	= localhost; charset = utf8", "root");
@@ -34,6 +34,129 @@ else {echo "<p>Заполните поля</p>";}
 }
 	
 
+
+function getTable(){
+	$nameBase = trim(strip_tags($_POST['nameBase']));
+	$pdo = new PDO("mysql:host 	= localhost; charset = utf8", "root");
+
+	if (!empty($nameBase)){
+	  $nameBase = $_POST['nameBase'];
+		$sql = "SHOW TABLES FROM $nameBase";
+		$result = $pdo -> query ($sql);
+
+
+  
+	foreach ($result as $row){
+		echo "<br>Таблица в $nameBase: <a href=\"?nameBase=$nameBase&nameTable=".$row[0] ."\"> $row[0]</a><br>";
+	}
+	} 
+ 
+  $nameTable = $_GET['nameTable'];
+  $nameBase = $_GET['nameBase'];
+
+  if (isset($nameTable) and isset($nameBase)){
+  $pdo = new PDO("mysql:host 	= localhost; dbname=$nameBase; charset = utf8", "root");
+	$sql = "DESCRIBE $nameTable";
+	$stmt = $pdo -> query ($sql);
+  // var_dump( $nameTable);
+
+
+
+	
+
+
+
+
+	echo "<table>";
+		echo "<thead>";
+			echo "<tr>";
+				echo "<td>Field</td>";
+				echo "<td>Type</td>";
+				echo "<td>Null</td>";
+				echo "<td>Key</td>";
+				echo "<td>Default</td>";
+				echo "<td>Extra</td>";
+			echo "</tr>";
+		echo "</thead>";
+
+		
+		foreach ($stmt as $row) {
+ 	// echo( "<tr> <td> $row[0] </td><td>$row[1] </td><td>$row[2]</td> <td>$row[3]</td><td> $row[4]</td><td> $row[5]</td> </tr>");
+			echo "<tr>";
+			for ($i=0; $i < 6 ; $i++) { 
+				echo( "<td> <a href=\"?nameBase=$nameBase&nameTable=$nameTable&nameCell=". $row[$i] . "\"> $row[$i] </a></td>");
+			}
+			echo "</tr>";
+
+		}
+
+
+$nameCell = $_GET['nameCell'];
+echo "<p>Кликните мышкой на ссылку в столбце Field для действием над строкой</p>";
+// var_dump($_POST);
+// var_dump($nameCell);
+
+if ($nameCell){
+
+
+if ($_POST['del']){
+// Удаляем строку в таблице
+$sql = "ALTER TABLE $nameTable DROP COLUMN $nameCell";
+$pdo -> query ($sql); 
+echo "<br>";
+echo "Вы удалили в таблице $nameTable поле:";
+echo "<br>";
+echo($nameCell);
+
+
+}
+else if ($_POST['editNameCell']){
+
+	
+
+echo "<form action='' method=POST>";
+echo "<lable>Введите новое название поля</lable>";
+echo "<br>";
+echo "<input type=text name = newNameCell>";
+echo "<br>";
+echo "<lable>Введите новый тип (INT, VARCHAR, TEXT, DATE)</lable>";
+echo "<br>";
+echo "<input type=text name = newTypeCell>";
+echo "<br>";
+echo "<input type=submit value=Изменить>	";	
+
+
+}
+
+if ($_POST['newNameCell'] and $_POST['newTypeCell']){
+	$newNameCell = $_POST['newNameCell'];
+  $newTypeCell = $_POST['newTypeCell'];
+	echo "ok";
+	$sql = "ALTER TABLE $nameTable CHANGE $nameCell $newNameCell $newTypeCell NOT NULL DEFAULT 0";
+  $pdo -> query ($sql); 
+}
+// var_dump($_POST);
+
+	
+	echo "</table>";
+	
+
+echo "<form action='' method = post>";
+
+	echo "<input type=submit value=Удалить поле name =del >";
+
+	echo "<input type=submit value=Изменить название поля или тип name =editNameCell>";
+
+	echo "<input type=submit value=Обновить таблицу name =update>";
+
+
+echo "</form>";
+
+
+}
+
+}
+}
 
 
 ?>
--- lesson_4.4/table.php
+++ lesson_4.4/table.php
@@ -1,4 +1,6 @@
-<?php ob_start(); ?>
+<?php ob_start(); 
+include __DIR__ . "/function.php";?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -29,128 +31,8 @@
 		<lable>Введите название базы содержимое которой вы хотите увидеть и нажмите Enter</lable>
 		<input type="text" name="nameBase">
 	</form>
-
-
-	<?php
-	$nameBase = trim(strip_tags($_POST['nameBase']));
-	$pdo = new PDO("mysql:host 	= localhost; charset = utf8", "root");
-
-	if (!empty($nameBase)){
-	  $nameBase = $_POST['nameBase'];
-		$sql = "SHOW TABLES FROM $nameBase";
-		$result = $pdo -> query ($sql);
-
-
-  
-	foreach ($result as $row){
-		echo "<br>Таблица в $nameBase: <a href=\"?nameBase=$nameBase&nameTable=".$row[0] ."\"> $row[0]</a><br>";
-	}
-	} 
- 
-  $nameTable = $_GET['nameTable'];
-  $nameBase = $_GET['nameBase'];
-
-  if (isset($nameTable) and isset($nameBase)){
-  $pdo = new PDO("mysql:host 	= localhost; dbname=$nameBase; charset = utf8", "root");
-	$sql = "DESCRIBE $nameTable";
-	$stmt = $pdo -> query ($sql);
-  // var_dump( $nameTable);
-
-
-
-	?>
-
-
-
-
-	<table>
-		<thead>
-			<tr>
-				<td>Field</td>
-				<td>Type</td>
-				<td>Null</td>
-				<td>Key</td>
-				<td>Default</td>
-				<td>Extra</td>
-			</tr>
-		</thead>
-
-		<?
-		foreach ($stmt as $row) {
- 	// echo( "<tr> <td> $row[0] </td><td>$row[1] </td><td>$row[2]</td> <td>$row[3]</td><td> $row[4]</td><td> $row[5]</td> </tr>");
-			echo "<tr>";
-			for ($i=0; $i < 6 ; $i++) { 
-				echo( "<td> <a href=\"?nameBase=$nameBase&nameTable=$nameTable&nameCell=". $row[$i] . "\"> $row[$i] </a></td>");
-			}
-			echo "</tr>";
-
-		}
-
-
-$nameCell = $_GET['nameCell'];
-echo "<p>Кликните мышкой на ссылку в столбце Field для действием над строкой</p>";
-// var_dump($_POST);
-// var_dump($nameCell);
-
-if ($nameCell){
-
-
-if ($_POST['del']){
-// Удаляем строку в таблице
-$sql = "ALTER TABLE $nameTable DROP COLUMN $nameCell";
-$pdo -> query ($sql); 
-echo "<br>";
-echo "Вы удалили в таблице $nameTable поле:";
-echo "<br>";
-echo($nameCell);
-
-
-}
-else if ($_POST['editNameCell']){
-
-	?>
-
-<form action="" method="POST">
-<lable>Введите новое название поля</lable>
-<br>
-<input type="text" name = 'newNameCell'>
-<br>
-<lable>Введите новый тип (INT, VARCHAR, TEXT, DATE)</lable>
-<br>
-<input type="text" name = 'newTypeCell'>
-<br>
-<input type="submit" value="Изменить">		
-<?
-
-}
-
-if ($_POST['newNameCell'] and $_POST['newTypeCell']){
-	$newNameCell = $_POST['newNameCell'];
-  $newTypeCell = $_POST['newTypeCell'];
-	echo "ok";
-	$sql = "ALTER TABLE $nameTable CHANGE $nameCell $newNameCell $newTypeCell NOT NULL DEFAULT 0";
-  $pdo -> query ($sql); 
-}
-// var_dump($_POST);
-
-		?>
-	</table>
-	
-
-<form action="" method = "post">
-	<input type="submit" value="Удалить поле" name ="del" >
-	<input type="submit" value="Изменить название поля или тип" name ="editNameCell">
-	<input type="submit" value="Обновить таблицу" name ="update">
-
-<!-- 	<input type="submit" value="Изменить тип поля" name ="editTypeCell"> -->
-</form>
-
-
-<?
-}
-
-}
-
-?>
+<?php 
+getTable();
+ ?>
 </body>
 </html>
