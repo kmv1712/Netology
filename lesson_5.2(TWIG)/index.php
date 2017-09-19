b8b95de5cbc1b05d15e5cae3592cfb3bc86b2b5b
kmv1712 <kmv1712@gmail.com>
2017-09-19 22:04:28 +0500

    Add

--- lesson_5.2(TWIG)/index.php
+++ lesson_5.2(TWIG)/index.php
@@ -10,34 +10,53 @@ $templ = $twig -> loadTemplate ('register.html');
 echo $templ -> render(array(
 	));
 
-if ($_POST and !empty($_POST['login']) and !empty($_POST['password']) and !empty($_POST['register'])){
-	print_r($_POST);
-	$pdo = new PDO("mysql:host 	= $host; dbname=$nameBase; charset = utf8", "$root", "$password");
+
+/////////////////////////// Регистрация//////////////////////////////////
+if ($_POST and !empty($_POST['login']) and !empty($_POST['register'])){
+	// print_r($_POST);
 	$userLogin = $_POST['login'];
 	$userPassword = $_POST['password'];
-
-	$sql = "INSERT INTO user VALUES ('',	'$userLogin' , '$userPassword')";
+	$pdo = new PDO("mysql:host 	= $host; dbname=$nameBase; charset = utf8", "$root", "$password");
+	$sql = "SELECT id FROM user WHERE login = '$userLogin'";
 	$stmt = $pdo -> query ($sql);
+	$coun = $stmt -> rowCount();
+	// echo "$coun";
+	if ($coun > 0) {
+		echo 'Логин занят, придумайте новый логин';
+	} else {
+		$pdo = new PDO("mysql:host 	= $host; dbname=$nameBase; charset = utf8", "$root", "$password");
+		$sql = "INSERT INTO user VALUES ('',	'$userLogin' , '$userPassword')";
+		$templ = $twig -> loadTemplate ('main_table_task.html');
+   	}
 }
 else 
 	{echo "Введите логин, пароль";}
+/////////////////////////////////////////////////////////////////////////
 
-$pdo = new PDO("mysql:host 	= $host; dbname=$nameBase; charset = utf8", "$root", "$password");
-$sql = "SELECT * FROM $nameTable";
+// 	$pdo = new PDO("mysql:host 	= $host; dbname=$nameBase; charset = utf8", "$root", "$password");
+// 	$userLogin = $_POST['login'];
+// 	$userPassword = $_POST['password'];
+// 	$sql = "INSERT INTO user VALUES ('',	'$userLogin' , '$userPassword')";
+// 	$stmt = $pdo -> query ($sql);
+// }
 
-$stmt = $pdo -> query ($sql);
-while ($row = $stmt->fetchObject()) {
-	$data[] = $row;
+////////////////////Выводим таблицу с задачи/////////////////////////////////
+// $pdo = new PDO("mysql:host 	= $host; dbname=$nameBase; charset = utf8", "$root", "$password");
+// $sql = "SELECT * FROM $nameTable";
 
-}
-unset($pdo);
+// $stmt = $pdo -> query ($sql);
+// while ($row = $stmt->fetchObject()) {
+// 	$data[] = $row;
 
-
-
-$templ = $twig -> loadTemplate ('main_table_task.html');
-echo $templ -> render(array(
-	'data' => $data
-	));
+// }
+// unset($pdo);
 
 
-?>
+
+// $templ = $twig -> loadTemplate ('main_table_task.html');
+// echo $templ -> render(array(
+// 	'data' => $data
+// 	));
+////////////////////////////////////////////////////////////////////
+
+	?>
