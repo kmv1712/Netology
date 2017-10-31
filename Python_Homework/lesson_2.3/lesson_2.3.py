d9858457ea6210856fc0f9dd96e6cd67c8b95e7e
kmv1712 <kmv1712@gmail.com>
2017-10-31 22:45:11 +0500

    add

--- Python_Homework/lesson_2.3/lesson_2.3.py
+++ Python_Homework/lesson_2.3/lesson_2.3.py
@@ -8,23 +8,21 @@ filename = ['newsafr.json', 'newscy.json', 'newsfr.json', 'newsit.json']
 
 
 def get_chardet(file):
-    top = []
-    with open(file, 'rb') as f:
-        data = json.load(f)
-        data = data['rss']['channel']['description']
-        # pprint(data)
-        data = re.sub('[^А-Яа-яA-Za-z\data\w]*', '', data)
-        # print (s)
-        data = data.replace("\r", " ").split(" ")
-        # print (s)
-        for word in data:
-            if len(word) > 6:
-                top.append(word)
-                c = Counter(top)
-    # print(top)
-    print("Топ 10 слов в файле", file, ":", "\n", c.most_common(10))
-
+  top = []
+  with open(file, 'rb') as f:
+      data = f.read() # прочитали bytes
+      res = chardet.detect(data)
+      decoded_string = data.decode(res['encoding']) # Декодируем байты в строку
+      # print(decoded_string)
+      data = decoded_string
+      data = re.sub('[^А-Яа-яA-Za-z\s\w]*', '', data)
+      data = data.replace("\r", " ").split(" ")
+      for word in data:
+          if len(word) > 6:
+              top.append(word)
+              c = Counter(top)
+  # print(top)
+  print("Топ 10 слов в файле", file, ":", "\n", c.most_common(10))
 
 for file in filename:
     get_chardet(file)
-
