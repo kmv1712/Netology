ea981b8d01bce9fc30410d9b9e42b77932f1888a
kmv1712 <kmv1712@gmail.com>
2017-10-31 23:10:25 +0500

    add

--- Python_Homework/lesson_2.3/lesson_2.3.py
+++ Python_Homework/lesson_2.3/lesson_2.3.py
@@ -14,12 +14,14 @@ def get_chardet(file):
       res = chardet.detect(data)
       decoded_string = data.decode(res['encoding']) # Декодируем байты в строку
       # print(decoded_string)
-      data = decoded_string
-      data = re.sub('[^А-Яа-яA-Za-z\s\w]*', '', data)
-      data = data.replace("\r", " ").split(" ")
+      data = json.loads(decoded_string)
+      # data = re.sub('[^А-Яа-яA-Za-z\s\w]*', '', data)
+      data = data ['rss']['channel']['items'][0]['description']
+      data = data.split(" ")
       for word in data:
           if len(word) > 6:
               top.append(word)
+              # print (top)
               c = Counter(top)
   # print(top)
   print("Топ 10 слов в файле", file, ":", "\n", c.most_common(10))
