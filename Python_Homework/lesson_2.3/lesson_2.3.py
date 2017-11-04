02b535c02aee67a1d18f9d219fab00e669575ad2
kmv1712 <kmv1712@gmail.com>
2017-11-04 22:54:28 +0500

    add

--- Python_Homework/lesson_2.3/lesson_2.3.py
+++ Python_Homework/lesson_2.3/lesson_2.3.py
@@ -8,23 +8,38 @@ filename = ['newsafr.json', 'newscy.json', 'newsfr.json', 'newsit.json']
 
 
 def get_chardet(file):
-  top = []
-  with open(file, 'rb') as f:
-      data = f.read() # прочитали bytes
-      res = chardet.detect(data)
-      decoded_string = data.decode(res['encoding']) # Декодируем байты в строку
-      # print(decoded_string)
-      data = json.loads(decoded_string)
-      # data = re.sub('[^А-Яа-яA-Za-z\s\w]*', '', data)
-      data = data ['rss']['channel']['items'][0]['description']
-      data = data.split(" ")
-      for word in data:
-          if len(word) > 6:
-              top.append(word)
-              # print (top)
-              c = Counter(top)
-  # print(top)
-  print("Топ 10 слов в файле", file, ":", "\n", c.most_common(10))
+    top = []
+    allDescription = []
+    description = []
+    with open(file, 'rb') as f:
+        data = f.read()  # прочитали bytes
+        res = chardet.detect(data)
+        decoded_string = data.decode(res['encoding'])  # Декодируем байты в строку
+        # print(decoded_string)
+        data = json.loads(decoded_string)
+        # pprint(data)
+        for i in range(40):
+            # print(data ['rss']['channel']['items'][i]['description'])
+            allDescription.append(data['rss']['channel']['items'][i]['description'])
+        # pprint(allDescription)
+        # allDescription = map(str.strip, allDescription)
+        # pprint(allDescription)
+        # allDescription = re.sub('[^А-Яа-яA-Za-z\s\w]*', '', allDescription)
+        # data = re.sub('[^А-Яа-яA-Za-z\s\w]*', '', data)
+        # data = data ['rss']['channel']['items'][0]['description']
+        for item in allDescription:
+            item = item.split(" ")
+            # print (item)
+            description.extend(item)
+        # allDescription.append(item)
+        # pprint(description)
+        for word in description:
+            if len(word) > 6:
+                top.append(word)
+                c = Counter(top)
+    # print(top)
+    print("Топ 10 слов в файле", file, ":", "\n", c.most_common(10))
+
 
 for file in filename:
     get_chardet(file)
