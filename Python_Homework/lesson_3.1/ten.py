c4ac90fcc19235d2cc65b4ac34d8e1820ea6400a
kmv1712 <kmv1712@gmail.com>
2017-11-15 20:44:18 +0500

    Add

--- Python_Homework/lesson_3.1/ten.py
+++ Python_Homework/lesson_3.1/ten.py
@@ -1,10 +1,11 @@
-# Задание отправил в 21:02 (по местному) в 19:02 (по Московскому) 06.08.2017
+# Задание отправил в 20:42 (по местному) в 18:42 (по Московскому) 15.11.2017
 
 mass = [x for x in range(1000)]
 list_one = []
+list_one = [x ** 3 for x in mass if x % 3 == 0 and x % 4 == 0 and x != 0]
 
-for item in mass:
-  if item%3 == 0 and item%4 == 0 and item != 0 :
-    list_one.append(item**3)
-    
+# for item in mass:
+#   if item%3 == 0 and item%4 == 0 and item != 0 :
+#     list_one.append(item**3)
+
 print(list_one)
