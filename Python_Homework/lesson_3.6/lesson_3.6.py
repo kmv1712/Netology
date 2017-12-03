7e930926a40fa65b84398a9ecaf2d60f739bcd1b
kmv1712 <kmv1712@gmail.com>
2017-12-03 17:07:05 +0500

    Add

--- Python_Homework/lesson_3.6/lesson_3.6.py
+++ Python_Homework/lesson_3.6/lesson_3.6.py
@@ -15,111 +15,111 @@ class Animals:
 
 
 class Cows(Animals):
-    name = "Корова"
     sound = "Му"
 
-    def __init__(self, weight, color):
+    def __init__(self, name, weight, color):
+        self.name = name
         self.weight = weight
         self.color = color
 
 
 class Goats(Animals):
-    name = "Коза"
     sound = "Ме-е"
 
-    def __init__(self, weight, color):
+    def __init__(self, name, weight, color):
+        self.name = name
         self.weight = weight
         self.color = color
 
 
 class Sheeps(Animals):
-    name = "Овца"
     sound = "Бе-е"
 
-    def __init__(self, weight, color):
+    def __init__(self, name, weight, color):
+        self.name = name
         self.weight = weight
         self.color = color
 
 
 class Pigs(Animals):
-    name = "Свинья"
     sound = "Хрю-Хрю"
 
-    def __init__(self, weight, color):
+    def __init__(self, name, weight, color):
+        self.name = name
         self.weight = weight
         self.color = color
 
 
 class Ducks(Animals):
-    name = "Утка"
     sound = "Кря-Кря"
 
-    def __init__(self, weight, color):
+    def __init__(self, name, weight, color):
+        self.name = name
         self.weight = weight
         self.color = color
 
 
 class Hens(Animals):
-    name = "Курица"
     sound = "Ко-Ко-Ко"
 
-    def __init__(self, weight, color):
+    def __init__(self, name, weight, color):
+        self.name = name
         self.weight = weight
         self.color = color
 
 
 class Geese(Animals):
-    name = "Гусь"
     sound = "Га-га-га"
 
-    def __init__(self, weight, color):
+    def __init__(self, name, weight, color):
+        self.name = name
         self.weight = weight
         self.color = color
 
 
-cow = Cows(100, 'белый')
+cow = Cows('Корова', 100, 'белый')
 cow.get_sound()
 cow.get_weight()
 cow.get_color()
 
 print()
 
-goat = Goats(30, 'бело-серый')
+goat = Goats('Овца', 30, 'бело-серый')
 goat.get_sound()
 goat.get_weight()
 goat.get_color()
 
 print()
 
-sheep = Sheeps(20, 'кремовый')
+sheep = Sheeps('Коза', 20, 'кремовый')
 sheep.get_sound()
 sheep.get_weight()
 sheep.get_color()
 
 print()
 
-pig = Pigs(40, 'розовый')
+pig = Pigs('Свинья', 40, 'розовый')
 pig.get_sound()
 pig.get_weight()
 pig.get_color()
 
 print()
 
-duck = Ducks(5, 'коричневый')
+duck = Ducks('Утка', 5, 'коричневый')
 duck.get_sound()
 duck.get_weight()
 duck.get_color()
 
 print()
 
-hen = Hens(3, 'коричневый')
+hen = Hens('Курица', 3, 'коричневый')
 hen.get_sound()
 hen.get_weight()
 hen.get_color()
 
 print()
 
-goose = Geese(6, 'белый')
+goose = Geese('Гусь', 6, 'белый')
 goose.get_sound()
 goose.get_weight()
 goose.get_color()
